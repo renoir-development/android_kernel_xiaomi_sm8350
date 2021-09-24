12af9698c871c2d467f308cdb9932e5245d16d07
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-24 20:27:49 +0300

    input: touchscreen: fts_spi: trigger fod hbm
    Change-Id: I7cc6615c41170a86c18a72058c90ddd55d55ad4d

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -55,6 +55,8 @@
 #include <linux/notifier.h>
 #include <linux/backlight.h>
 #include <drm/drm_panel.h>
+#include <drm/dsi_display_fod.h>
+
 #include <linux/fb.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
@@ -176,6 +178,7 @@ void release_all_touches(struct fts_ts_info *info)
 #ifdef CONFIG_FTS_BOOST
 	lpm_disable_for_dev(false, EVENT_INPUT);
 #endif
+	dsi_display_primary_request_fod_hbm(0);
 	info->touch_id = 0;
 	info->touch_skip = 0;
 	info->fod_id = 0;
@@ -3534,10 +3537,14 @@ static void fts_enter_pointer_event_handler(struct fts_ts_info *info,
 				__set_bit(touchId, &info->fod_id);
 				input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, info->fod_overlap);
 				input_report_key(info->input_dev, BTN_INFO, 1);
+				input_sync(info->input_dev);
+				dsi_display_primary_request_fod_hbm(1);
 			}
 		} else if (__test_and_clear_bit(touchId, &info->fod_id)) {
 			input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, 0);
 			input_report_key(info->input_dev, BTN_INFO, 0);
+			input_sync(info->input_dev);
+			dsi_display_primary_request_fod_hbm(0);
 			info->fod_x = 0;
 			info->fod_y = 0;
 			info->fod_coordinate_update = false;
@@ -3646,6 +3653,8 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 	if (__test_and_clear_bit(touchId, &info->fod_id)) {
 		input_report_abs(info->input_dev, ABS_MT_WIDTH_MINOR, 0);
 		input_report_key(info->input_dev, BTN_INFO, 0);
+		input_sync(info->input_dev);
+		dsi_display_primary_request_fod_hbm(0);
 		info->fod_coordinate_update = false;
 		info->fod_x = 0;
 		info->fod_y = 0;
@@ -3661,6 +3670,8 @@ static void fts_leave_pointer_event_handler(struct fts_ts_info *info,
 
 		info->fod_pressed = false;
 		input_report_key(info->input_dev, BTN_INFO, 0);
+		input_sync(info->input_dev);
+		dsi_display_primary_request_fod_hbm(0);
 
 #ifdef FTS_XIAOMI_TOUCHFEATURE
 		wake_up(&info->wait_queue);
@@ -4051,6 +4062,7 @@ static void fts_gesture_event_handler(struct fts_ts_info *info,
 					info->fod_pressed = true;
 					input_report_key(info->input_dev, BTN_INFO, 1);
 					input_sync(info->input_dev);
+					dsi_display_primary_request_fod_hbm(1);
 					if (info->fod_id) {
 						fod_id = ffs(info->fod_id) - 1;
 						if (info->fod_id & ~(1 << fod_id))
