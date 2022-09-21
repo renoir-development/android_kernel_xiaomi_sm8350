7a610080e1f4d4834ba636b2f9edb12aae6b87ce
Arian <arian.kulmer@web.de>
2022-09-21 13:44:06 +0200

    input: touchscreen: focaltech_spi: Add xiaomi,k9-spi to compatible dt
    Change-Id: I1052de6d01254fcd76f9a16f5e415af6ceb23325

--- drivers/input/touchscreen/focaltech_spi/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_spi/focaltech_core.c
@@ -2674,6 +2674,7 @@ static const struct spi_device_id fts_ts_id[] = {
 };
 static const struct of_device_id fts_dt_match[] = {
 	{.compatible = "focaltech,fts", },
+	{.compatible = "xiaomi,k9-spi", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, fts_dt_match);
